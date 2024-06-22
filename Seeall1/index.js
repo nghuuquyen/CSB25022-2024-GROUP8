8b066c4654a5d2ab23434a847b937788674e3d65
= <=>
2024-06-22 17:15:27 +0700

    Code Shopvt.com

---
+++ Seeall1/index.js
@@ -0,0 +1,25 @@
+document.querySelectorAll('.custom-select').forEach(function(select) {
+    select.addEventListener('change', function() {
+      console.log('Selected value:', this.value);
+    });
+  });
+document.querySelectorAll('.custom-rank').forEach(function(select) {
+    select.addEventListener('change', function() {
+      console.log('Selected value:', this.value);
+    });
+  });
+document.querySelectorAll('.custom-characters').forEach(function(select) {
+    select.addEventListener('change', function() {
+      console.log('Selected value:', this.value);
+    });
+  });
+document.querySelectorAll('.custom-skins').forEach(function(select) {
+    select.addEventListener('change', function() {
+      console.log('Selected value:', this.value);
+    });
+  });
+document.querySelectorAll('.custom-sorted').forEach(function(select) {
+    select.addEventListener('change', function() {
+      console.log('Selected value:', this.value);
+    });
+  });
